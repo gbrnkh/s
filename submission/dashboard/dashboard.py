dccb511044dee45e0c88baa7a67e6308d6d0921a
gbrnkh <142367890+gbrnkh@users.noreply.github.com>
2025-03-11 00:37:29 +0700

    Update dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -1,7 +1,7 @@
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
 
 # Memuat dataset
 day_df = pd.read_csv("./data/day.csv")
